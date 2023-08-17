c12daefea25a63d48d0e2b370538e9626d55d3b0
crizrom <99371631+crizrom@users.noreply.github.com>
2023-08-17 01:31:30 -0600

    modified cpp file

---
+++ cpptest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+
+    std::cout << "LA VARIABLE X ES: " << x << '\n';
+    std::cout << "LA VARIABLE Y ES: "<< y << '\n';
+    std::cout << "LA SUMA DE AMBAS VARIABLES ES: "<< sum << '\n';
+
+    return 0;
+}
