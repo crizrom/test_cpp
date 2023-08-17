d552a5c5493d8d3c787026537abc9fe6bb15aca7
crizrom <99371631+crizrom@users.noreply.github.com>
2023-08-17 02:18:06 -0600

    New files for cpp test repo

---
+++ functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void github(){
+    std::cout << "this is a github aimed test file" << std::endl;
+    std::cout << "this is a github aimed test file" << std::endl;
+    std::cout << "this is a github aimed test file" << std::endl;
+    std::cout << "this is a github aimed test file" << std::endl;
+}
+int main()
+{
+    github();
+    return 0; 
+}
