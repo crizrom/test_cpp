3191cf99bc01b8a76adb0f31b1a53b67b1c27c1a
crizrom <99371631+crizrom@users.noreply.github.com>
2023-08-18 11:22:19 -0600

    Prime numbers file works now, namespace just for testing

--- namespace.cpp
+++ namespace.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
 int main(){
-    int x;
-    std::cout << "ingresa numero";
-    std::cin >> x;
-    std::cout << x; 
+    double x = 5;
+    double y = 3;
+    double z;
+    //std::cout << "ingresa numero";
+    //std::cin >> x;
+    //std::cout << "ingresa numero";
+    //std::cin >> x;
+    z = x/y;
+    std::cout << z;
     return 0;
 }
--- prime_number.cpp
+++ prime_number.cpp
@@ -6,28 +6,46 @@ int main(){
     int low;
     int upper;
 
-    std::cout << "DIVIDE NUMBER IN RANGE" << '\n';
+    std::cout << "PRIME NUMBER IN RANGE" << '\n';
     std::cout << "------------------------------------------" << '\n';
     std::cout << "Introduce a lower limit and an upper limit" << '\n';
     
-    std::cout << "LOWER LIMIT: ";
+    //std::cout << "LOWER LIMIT (except negative numbers and 1): ";
+    do{
+    std::cout << "LOWER LIMIT (except negative numbers and 1): ";    
     std::cin >> low;
-    std::cout << "UPPER LIMIT: ";
+    }while(low <= 1);
+
+    do{
+    std::cout << "UPPER LIMIT (except negative numbers and 1): ";
     std::cin >> upper;
+    }while(upper <=1 );
+
     range(low, upper);
  
     return 0;
 }
 
 void range(int low, int upper){
-    double y;
+    int y;
     for (int i = low; i <= upper; i++){
+        int count = 0;
         for (int z = low; z <= upper; z++){
-            y = i/z;
-            std::cout << y << " ";
+            //y = i/z;
+            y = i%z;
+            //int count = 0;
+            if(y == 0){
+            //std::cout << "*";
+            count++;
+            }
         }
+
+        if(count == 1){
+                    std::cout << i << '\n';
+                    //std::cout << " " << count;
+                }
         
-        std::cout << '\n';
+        //std::cout << '\n';
     }
 
 }
